207950eb78a5b3689d97ebe8c677cdd8e2690a5b
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-09-04 09:31:24 +0200

    har laggt in add sub och modulo kod

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int tal1 = 2;
+        int tal2 = 30;
+
+        int summa_addition = tal1 + tal2;
+        int summa_subtraktion = tal2 - tal1;
+
+        System.out.println("Summan av tal1 och tal2 är "+summa_addition);
+        System.out.println("Diferensen av tal2 och tal1 "+summa_subtraktion);
+
+        int rest1 = 10%3;
+        int rest2 = 9%3;
+
+        }
+}
